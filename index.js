b126b9db9dc90b63d9da9da74bdc69bb78d2dfd4
tormasa <matti.a.torvinen@gmail.com>
2021-05-09 16:34:59 +0300

    implement group stage selection

--- index.js
+++ index.js
@@ -5,34 +5,90 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
-document.getElementById("alkulohkoButton").addEventListener("click", openAlkulohko);
-document.getElementById("backButtonAlkulohko").addEventListener("click", openMain);
-document.getElementById("sendButton").addEventListener("click", send);
+let teamData;
+let qualGroupTeams = [];
+
+request.onload = function() {
+    teamData = request.response;
+    // Alkulohkojen luonti kun on ladattu json
+    for (var i = 0; i < 6; i++) {
+        createLohko((i+10).toString(36).toUpperCase(), teamData, i % 2 != 0);
+    }
+
+    // Lisätään toiminnallisuudet painikkeisiin vasta kun on saatu vastaus json-viestiin
+    document.getElementById("alkulohkoButton").addEventListener("click", openAlkulohko);
+    document.getElementById("backButtonAlkulohko").addEventListener("click", openMainFromGroup);
+    document.getElementById("sendButton").addEventListener("click", send);
+}
+
+
 
 document.getElementById("mainBlock").style.display = "block";
 document.getElementById("alkulohkoBlock").style.display = "none";
 
-// Alkulohkojen luonti
-for (var i = 0; i < 1; i++) {
-    createLohko((i+10).toString(36).toUpperCase());
-}
-
-
 function openAlkulohko() {
     document.getElementById("mainBlock").style.display = "none";
     document.getElementById("alkulohkoBlock").style.display = "block";
+    document.getElementById("groupError").innerText = " ";
 }
 
-function openMain() {
-    document.getElementById("mainBlock").style.display = "block";
-    document.getElementById("alkulohkoBlock").style.display = "none";
+function openMainFromGroup() {
+    var formElements = document.getElementById("groupForm").elements;
+    qualGroupTeams = [];
+
+    for (var i = 0; i < formElements.length; i++) {
+        if (formElements[i].checked) {
+            qualGroupTeams.push(i);
+        }
+    }
+
+    var count = qualGroupTeams.length;
+
+    if (count < 16) {
+        document.getElementById("groupError").innerText = "LIIAN VÄHÄN VALITTUJA JOUKKUEITA!";
+    }
+    else if (count > 16) {
+        document.getElementById("groupError").innerText = "LIIAN PALJON VALITTUJA JOUKKUEITA!";
+    }
+    else {
+        const teams = teamData['teams'];
+        var valid = true;
+
+        // Käydään lohkot läpi
+        for (var i = 0; i < 6; i++) {
+            var groupCount = 0;
+            //console.log("group " + (i+10).toString(36).toUpperCase());
+
+            for (var j = 0; j < qualGroupTeams.length; j++) {    
+                //console.log("value: " +formElements[j].value);
+                if (teams[qualGroupTeams[j]].lohko == (i+10).toString(36).toUpperCase()) {
+                    groupCount++;
+                }
+            }
+            
+            if (groupCount > 3) {
+                document.getElementById("groupError").innerText = "YHDESTÄ LOHKOSTA VOI PÄÄSTÄ JATKOON VAIN KOLME PARASTA!";
+                valid = false;
+
+                break;
+            }
+        }
+
+        if (valid) {
+            document.getElementById("mainBlock").style.display = "block";
+            document.getElementById("alkulohkoBlock").style.display = "none";
+        }
+    }
+    
 }
 
 function send() {
     console.log(document.getElementById("veikkaajanNimi").value);
 }
 
-function createLohko(kirjain) {
+function createLohko(kirjain, obj, pair) {
+    const teams = obj['teams'];
+
     var div = document.createElement("div");
     var lohkoHeader = document.createElement("h3");
     var lohkoText = document.createTextNode("LOHKO " +kirjain);
@@ -43,27 +99,28 @@ function createLohko(kirjain) {
 
     // Joukkueet
     //var t1Button = document
-    loadJSON(function(response) {
-        // Parse JSON string into object
-        var actual_JSON = JSON.parse(response);
-        console.log(actual_JSON);
-    });
-    
-    var alkulohkoBlock = document.getElementById("alkulohkoBlock");
-    var backButton = document.getElementById("backButtonAlkulohko");
-    alkulohkoBlock.insertBefore(div, backButton);
-}
+    for (var i = 0; i < teams.length; i++) {
+        if (teams[i].lohko == kirjain) {
+            var container = document.createElement("label");
+            var checkBox = document.createElement("INPUT");
+            var checkMark = document.createElement("span");
 
-function loadJSON(callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'teams.json', true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            console.log(xobj.responseText);
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
+            container.innerHTML = teams[i].name;
+
+            //button.innerHTML = teams[i].name;
+            checkBox.setAttribute("type", "checkbox");
+
+            container.className = "container";
+            checkMark.className = "checkmark";
+            
+            container.appendChild(checkBox);
+            container.appendChild(checkMark);
+            div.appendChild(container);
+
+            checkBox.value = i;
         }
-    };
-    xobj.send(null);  
- }
+    }
+    
+    var parent = (pair) ? document.getElementById("groupPairCol") : document.getElementById("groupPairlessCol");
+    parent.appendChild(div);
+}
