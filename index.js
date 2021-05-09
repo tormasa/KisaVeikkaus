14ca6130ba8e6a022370fb98ed785ebf889b27de
tormasa <matti.a.torvinen@gmail.com>
2021-05-09 13:35:08 +0300

    add group stage screen

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+let requestURL = '';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+
 document.getElementById("alkulohkoButton").addEventListener("click", openAlkulohko);
-document.getElementById("backToMainButton").addEventListener("click", openMain);
+document.getElementById("backButtonAlkulohko").addEventListener("click", openMain);
 document.getElementById("sendButton").addEventListener("click", send);
 
 document.getElementById("mainBlock").style.display = "block";
 document.getElementById("alkulohkoBlock").style.display = "none";
+
+// Alkulohkojen luonti
+for (var i = 0; i < 1; i++) {
+    createLohko((i+10).toString(36).toUpperCase());
+}
+
 
 function openAlkulohko() {
     document.getElementById("mainBlock").style.display = "none";
@@ -18,3 +28,39 @@ function openMain() {
 function send() {
     console.log(document.getElementById("veikkaajanNimi").value);
 }
+
+function createLohko(kirjain) {
+    var div = document.createElement("div");
+    var lohkoHeader = document.createElement("h3");
+    var lohkoText = document.createTextNode("LOHKO " +kirjain);
+
+    // Otsake
+    lohkoHeader.appendChild(lohkoText);
+    div.appendChild(lohkoHeader);
+
+    // Joukkueet
+    //var t1Button = document
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        var actual_JSON = JSON.parse(response);
+        console.log(actual_JSON);
+    });
+    
+    var alkulohkoBlock = document.getElementById("alkulohkoBlock");
+    var backButton = document.getElementById("backButtonAlkulohko");
+    alkulohkoBlock.insertBefore(div, backButton);
+}
+
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'teams.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            console.log(xobj.responseText);
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);  
+ }
